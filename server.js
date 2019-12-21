d2f633b963b8c5a1e0df806e487842030a925389
Samuel <sam99dave@gmail.com>
2019-12-21 23:51:47 +0530

    milestone 2 reached

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+const db = require("./config/default").mongoURI;
+
+mongoose.Promise = global.Promise;
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 const app = express();
 
@@ -8,7 +19,10 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => res.render("index"));
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.set("views", path.join(__dirname, "views"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
