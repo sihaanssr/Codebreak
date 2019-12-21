b14ae54def59354a12299bec37d523440c78a23f
Samuel <sam99dave@gmail.com>
2019-12-21 19:11:19 +0530

    Node app done

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("index"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
