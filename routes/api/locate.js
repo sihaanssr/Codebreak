68a21b68b25c5b2944f541f79ee68a0373b0ac06
Samuel <sam99dave@gmail.com>
2019-12-22 11:46:45 +0530

    Find Others Demo Done

--- routes/api/locate.js
+++ routes/api/locate.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 const getcoordinates = require("../../validation/search");
 
-router.get("/dhundo", (req, res) => {
+/*router.get("/dhundo", (req, res) => {
   const coordinates = getcoordinates();
   console.log(coordinates);
   res.send("test");
+});*/
+
+router.get("/dhundo", (req, res) => {
+  res.render("others");
 });
 
 module.exports = router;
